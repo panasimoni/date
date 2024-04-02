cce991f1f428cd9952601f7326ebd0a13a034df9
simon <simonescuela9@gmail.com>
2024-04-02 10:05:10 +0100

    __gt__ realizado

--- date.py
+++ date.py
@@ -77,7 +77,9 @@ class Date:
         return self.get_delta_days() < other.get_delta_days()
 
     def __gt__(self, other) -> bool:
-        ...
+        if not isinstance(other, Date):
+            raise TypeError("La comparación solo es válida entre fechas")
+        return self.get_delta_days() > other.get_delta_days()
 
     def __eq__(self, other) -> bool:
         ...
